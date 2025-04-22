483c8f8b89e7e0dd6c167895ef30f54daa601ad7
manel <manelzaum@icloud.com>
2025-04-22 20:47:57 -0300

    feat: add serializers.

---
+++ api/serializers.py
@@ -0,0 +1,80 @@
+from rest_framework import serializers
+from core.models import *
+
+
+class StateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = State
+        fields = "__all__"
+
+
+class CitySerializer(serializers.ModelSerializer):
+    state = StateSerializer(read_only=True)
+    state_id = serializers.PrimaryKeyRelatedField(
+        queryset=State.objects.all(), write_only=True
+    )  # Aceita o ID de State para criação
+
+    class Meta:
+        model = City
+        fields = "__all__"
+
+
+class NeighborhoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Neighborhood
+        fields = "__all__"
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = "__all__"
+
+
+class UserTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserType
+        fields = "__all__"
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ("password",)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class ColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Color
+        fields = "__all__"
+
+
+class MetricSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Metric
+        fields = "__all__"
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+
+class SaleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sale
+        fields = "__all__"
+
+
+class SoldProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SoldProduct
+        fields = "__all__"
+
