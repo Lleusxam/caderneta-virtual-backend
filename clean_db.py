effc37922770b2cfde4f00d33ee67608b2393c16
MarlisonSilva <marlisonsoaresjp@gmail.com>
2025-06-12 10:31:58 -0300

    fix: removing Capacity from populate and clean db

--- clean_db.py
+++ clean_db.py
@@ -1,10 +1,10 @@
 from core.models import (State, City, Neighborhood, Address, UserType, User, Consortium,
-                        ConsortiumParticipation, Category, Color, Metric, Capacity,
+                        ConsortiumParticipation, Category, Color, Metric,
                         Product, Sale, SaleProduct, Billing, Payment, SoldProduct)
 
 
 models = [Payment, Billing, SoldProduct, SaleProduct, Sale,
-          Product, Capacity, Metric, Color, Category,
+          Product, Metric, Color, Category,
           ConsortiumParticipation, Consortium,
           User, UserType, Address, Neighborhood, City, State]
 
